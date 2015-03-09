72ca92305eb5327192cf368f36ab142ee087ac70
Newton <anthpaul0@gmail.com>
2015-03-09 15:48:37 -0500

    two tests

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:admin) { Admin.new(
+    email:                    'email@gmail.com',
+    password:                 'password',
+    password_confirmation:    'password'
+  )}
+
+  it 'should be valid' do
+    expect(admin).to be_valid
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:admin) { Admin.new( 
+                  email:                  'email@gmail.com',
+                  password:               'foobarfoo',
+                  password_confirmation:  'foobarfoo'
+  )}
+
+  let!(:post) { admin.post.new(
+                  title:    'a title',
+                  body:     'a body'
+  )}
+
+  xit 'should be valid' do
+    expect(post).to be_valid
+  end
 end
+
