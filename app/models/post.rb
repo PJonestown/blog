6565915d3d66bdc3fa09f14358265e0242a7a435
Newton <anthpaul0@gmail.com>
2015-03-09 20:30:20 -0500

    post title and body presence

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :admin
+
+  validates :title, :body, :presence => true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,16 +4,24 @@ RSpec.describe Post, type: :model do
   let(:admin) { Admin.new( 
                   email:                  'email@gmail.com',
                   password:               'foobarfoo',
-                  password_confirmation:  'foobarfoo'
-  )}
+                  password_confirmation:  'foobarfoo'        )}
 
   let!(:post) { admin.posts.new(
                   title:    'a title',
-                  body:     'a body'
-  )}
+                  body:     'a body'                         )}
 
   it 'should be valid' do
     expect(post).to be_valid
+  end
+
+  it 'should have a title' do
+    post.title = nil
+    expect(post).not_to be_valid
+  end
+
+  it 'should have a body' do
+    post.body = nil
+    expect(post).not_to be_valid
   end
 end
 
