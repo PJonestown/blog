de53ecd30065c756379dfebaf6b505edd61c8216
Newton <anthpaul0@gmail.com>
2015-03-10 16:18:31 -0500

    Use factorygirl in admin model spec

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -2,21 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
 
-  let(:admin) { Admin.new(
-    email:                    'email@gmail.com',
-    password:                 'password',
-    password_confirmation:    'password'
-  )}
+  #let(:admin) { Admin.new(
+   # email:                    'email@gmail.com',
+    #password:                 'password',
+    #password_confirmation:    'password'
+  #)}
+  let(:admin) { build(:admin) }
 
   it 'has a valid factory' do 
-    expect(build(:admin)).to be_valid
+    expect(admin).to be_valid
   end
 
-  it 'should be valid' do
+  xit 'should be valid' do
     expect(admin).to be_valid
   end
 
  it 'should return an array of posts' do
+   #build(:admin)
    expect(admin.posts).to eq([])
  end
 end
