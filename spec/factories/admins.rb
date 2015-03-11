a81f7f39f0ee3cee59de312ab7575e725b86e2af
Newton <anthpaul0@gmail.com>
2015-03-11 13:24:55 -0500

    changed mind

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -6,13 +6,5 @@ FactoryGirl.define do
     password { password }
     password_confirmation { password }
 
-    after(:build) do |admin|
-      admin.posts << build(:phone)
-    end
   end
-
-  factory :invalid_admin do
-    email nil
-  end
-
 end
