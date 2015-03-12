5957ec487f9f0576f962286deedd1c040813d927
Newton <anthpaul0@gmail.com>
2015-03-12 12:36:25 -0500

    added todo

--- spec/features/post_editing_spec.rb
+++ spec/features/post_editing_spec.rb
@@ -34,6 +34,18 @@ feature "Editing a post" do
       sign_out
       expect(page).not_to have_link('Edit')
 
+      #save_and_open_page
+      #todo this will always pass when using Rrunner by itself
+      #however running the full test suite in terminal can lead to failures
+      #The post id will either be 2, 3, or 5
+      #I think it depends on the order of feature tests
+      #so it will only pass if these feature tests are run first
+      #fail with id 3 if create post is run first
+      #and fail id 5 if create and delete run first
+      #which explains why I have to use 2 
+      #which means I set up database_cleaner incorrectly
+
+
       visit edit_post_path(2) #todo why isn't this 1? Why is there no 1
       expect(current_path).to eq(root_path)
       expect(page).to have_content('No soup for you!')
