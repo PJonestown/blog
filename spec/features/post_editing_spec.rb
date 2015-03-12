1dce017328db45981c93844e465b2201049fc8ec
Newton <anthpaul0@gmail.com>
2015-03-11 20:22:38 -0500

    new_post helper & lots of todos

--- spec/features/post_editing_spec.rb
+++ spec/features/post_editing_spec.rb
@@ -1,13 +1,18 @@
 require "Rails_helper"
+include LoginMacros
 
 feature "Editing a post" do
   background do
+
+    admin = create(:admin)
+    sign_in admin
+
+    new_post
 
   end
   context "Logged in admin" do
     scenario "Edits post" do
 
-      admin = create(:admin)
 
       visit_root_path
     end
--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,9 +1,24 @@
 module LoginMacros
 
+  #todo change file name to integration_helper
+  #module IntegrationHelper
+  #
+  #todo include Integration helper in all dependent files
+  #
+  #todo change to Integration helper in rails_helper
+
   def sign_in(admin)
     visit new_admin_session_path
     fill_in 'Email', with: admin.email
     fill_in 'Password', with: admin.password
     click_button 'Log in'
+  end
+
+  def new_post
+    visit root_path
+    click_link 'New Post'
+    fill_in title, with: 'A new title' #todo factory?
+    fill_in body, with: 'A new body'
+    click_button 'Create Post'
   end
 end
