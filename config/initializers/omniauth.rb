fe1e10cd90d305041caec80cde0dc3eeded3c636
Newton <anthpaul0@gmail.com>
2015-03-13 14:47:48 -0500

    Fixed typo

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-Rails.application.config.middleware.use Omniauth::Builder do
+Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, "KFl9vS8Yk6HmOTj6yIusVnlKy",
     "qJDrFnGXl2rUuZj66Pga9C3peFukVpWXgcOgQF6OBBuFts4pDX"
 end
