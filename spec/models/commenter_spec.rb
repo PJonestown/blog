50376fb885e5c538198f309b9b9d60b08ab5842a
Newton <anthpaul0@gmail.com>
2015-03-15 16:38:50 -0500

    Deleted spec

--- spec/models/commenter_spec.rb
+++ spec/models/commenter_spec.rb
@@ -11,9 +11,5 @@ RSpec.describe Commenter, type: :model do
     expect(commenter.identities).to eq([])
   end
 
-  it "is invalid without an identity" do
-    commenter.identities = nil
-    expect(commenter.identities).not_to be_valid
-  end
 end
 
