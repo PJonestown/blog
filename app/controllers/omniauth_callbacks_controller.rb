b9bd9f3014daabd52518aa4b5f556de374d1fa2e
Newton <anthpaul0@gmail.com>
2015-03-16 13:58:32 -0500

    Ridiculous commit. a lot

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,3 +1,19 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  #def self.provides_callback_for(provider)
+  def all
+
+    @commenter = Commenter.find_for_oath(env["omniauth.auth"],
+                                         current_commenter)
+    if @commenter.persisted?
+      
+    else
+
+      session["devise.#{provider}_data"] = env["omniath_auth"]
+      redirect_to new_commenter_registration_path
+    end
+
+  end
+
+  alias_method :twitter, :all
 
 end
--- app/models/commenter.rb
+++ app/models/commenter.rb
@@ -5,4 +5,38 @@ class Commenter < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :identities
+
+  TEMP_EMAIL_PREFIX = 'change@me'
+
+  def self.find_for_oath(auth, signed_in_resource = nil)
+
+    identity = Identity.find_for_oath(auth)
+
+    commenter = signed_in_resource ? signed_in_resource : identity.commenter
+
+    if commenter.nil?
+
+      email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
+
+      email = authinfo.email if email_is_verified
+
+      commenter = Comenter.where(:email => email).first if email
+
+      if commenter.nil?
+        commenter = Commenter.new(
+          email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
+          password: Devise.friendly_token[0,20]
+        )
+        commenter.skip_confirmation!
+        commenter.save
+      end
+
+    end
+
+    if identity.commenter != commenter
+      identity.commenter = commenter
+      identity.save!
+    end
+    commenter
+  end
 end
