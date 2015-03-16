c1f41929d43062a720757e2d6d691417e81dbf9c
Newton <anthpaul0@gmail.com>
2015-03-16 13:20:03 -0500

    identity validations and find method

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,3 +1,9 @@
 class Identity < ActiveRecord::Base
   belongs_to :commenter
+  validates_presence_of :uid, :provider
+  validates_uniqueness_of :uid, :scope => :provider
+
+  def self.find_for_oath(auth)
+    find_or_create_by(uid: auth.uid, provider: auth.provider)
+  end
 end
