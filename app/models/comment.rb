9d92aed2d2d7c380621e68ec2bbbc6d0dacb23f0
Newton <anthpaul0@gmail.com>
2015-03-17 15:30:32 -0500

    Finished polymorphic relationship

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :commenter
+
+  belongs_to :post, :polymorphic => true
+
+  belongs_to :commentable, :polymorphic => true
+  has_many :comments, :as => :commentable
+
 end
