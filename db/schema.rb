e798d1f956605e6b6ce1941dd71d82555a398d14
Newton <anthpaul0@gmail.com>
2015-03-17 14:46:14 -0500

    Add ancestry column and index to comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317194114) do
+ActiveRecord::Schema.define(version: 20150317194345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,8 +60,10 @@ ActiveRecord::Schema.define(version: 20150317194114) do
     t.integer  "commenter_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "ancestry"
   end
 
+  add_index "comments", ["ancestry"], name: "index_comments_on_ancestry", using: :btree
   add_index "comments", ["commenter_id"], name: "index_comments_on_commenter_id", using: :btree
 
   create_table "identities", force: :cascade do |t|
