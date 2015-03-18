b66ce5c7bdbb7a2ffb0f469ae46a234ea6c88485
Newton <anthpaul0@gmail.com>
2015-03-18 14:25:26 -0500

    comments helper

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,7 @@
 module CommentsHelper
+  def nested_comments(comments)
+    comments.map do |comment, sub_comments|
+      content_tag(:div, render(comment), :class => 'media') #todo
+    end.join.html_safe
+  end
 end
