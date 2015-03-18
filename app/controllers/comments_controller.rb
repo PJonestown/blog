fef60633b75dd6f1c4d41fda181e25251d0dc427
Newton <anthpaul0@gmail.com>
2015-03-18 18:47:26 -0500

    Updated comment params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -91,7 +91,13 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:body, :commentable_id, :commentable_type, :commenter_id, )
+      params.require(:comment).permit(
+        :body, 
+        :commentable_id, 
+        :commentable_type, 
+        :owner_type,
+        :owner_id 
+      )
     end
 
     def find_commentable
