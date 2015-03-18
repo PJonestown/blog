99cbcc1bf2cd5396343470ae701d311e21a7fec7
Newton <anthpaul0@gmail.com>
2015-03-18 14:32:20 -0500

    define coments in posts#show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,7 @@ class PostsController < ApplicationController
     @comment = @post.comments.build(params[:comment])
     @comment.commentable_type = 'Post'
     @comment.commenter = current_commenter
+    @comments = @post.comments
   end
 
   # GET /posts/new
