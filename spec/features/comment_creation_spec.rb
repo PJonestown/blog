17580595f019746d0f322545c889ce1c068c6343
Newton <anthpaul0@gmail.com>
2015-03-18 20:56:21 -0500

    admin create post

--- spec/features/comment_creation_spec.rb
+++ spec/features/comment_creation_spec.rb
@@ -22,10 +22,24 @@ feature 'comment creation on posts' do
       fill_in 'Body', with: 'A brand new comment'
       click_button 'Create Comment'
       expect(current_path).to eq(post_path(@new_post))
-      save_and_open_page
       expect(page).to have_content('A brand new comment')
   
 
+    end
+  end
+
+  context 'admin logged in' do
+    background do
+      admin = create(:admin)
+      sign_in(admin)
+    end
+
+    scenario 'adds a new comment' do
+      visit post_path(@new_post)
+      fill_in 'Body', with: 'Admins can comment too!'
+      click_button 'Create Comment'
+      expect(current_path).to eq(post_path(@new_post))
+      expect(page).to have_content('Admins can comment too!')
     end
   end
 end
