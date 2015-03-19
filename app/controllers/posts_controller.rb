1d937efb432ab229a169b1ff01dad690bc28e010
Newton <anthpaul0@gmail.com>
2015-03-18 20:10:27 -0500

    Clean up posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,55 +1,30 @@
 class PostsController < ApplicationController
-  #include Blag::Commentable
 
   include Commentable
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_admin!, :except =>[:show, :index] 
 
-  # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.all.order("created_at DESC")
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
   def show
+
     #todo why isn't this already defined in module?
-    #
-    @owner = 
-      if admin_signed_in?
-        type = 'Admin'
-        id = 1
-      elsif
-        commenter_signed_in?
-        type = 'Commenter'
-        id = @commenter.id
-      end
 
     @commentable = find_commentable
     @comments = @commentable.comments
     @comment = Comment.new 
-    
-    #@comment = @post.comments.build(params[:comment])
-    #@comment.commentable_type = 'Post'
-    #@comment.commenter = current_commenter
-    #@comments = @post.comments
-
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts
-  # POST /posts.json
   def create
-    #@post = Post.new(post_params)
     @post = current_admin.posts.new(post_params)
 
     respond_to do |format|
@@ -63,8 +38,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -77,8 +50,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
   def destroy
     @post = current_admin.posts.find(params[:id])
     if @post.admin == current_admin
@@ -94,12 +65,10 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
       params.require(:post).permit(:title, :body, :admin_id)
     end
