43b0df9ba5ffee2baaf4c26f5a1673c7e178705a
Newton <anthpaul0@gmail.com>
2015-03-19 14:03:51 -0500

    redefine authenticate_commenter!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def authenticate_commenter!
+    return if admin_signed_in?
+    super
+  end
+
 end
