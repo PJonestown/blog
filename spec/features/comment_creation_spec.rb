036b95dc50e348a2683e23b105ea7ce2ffc5ca32
Newton <anthpaul0@gmail.com>
2015-03-21 16:16:39 -0500

    Commenter commenting on comment integration test

--- spec/features/comment_creation_spec.rb
+++ spec/features/comment_creation_spec.rb
@@ -58,5 +58,40 @@ feature 'comment creation' do
       end
     end
   end
+
+  context 'on comments' do
+
+    background do 
+      @new_post = create(:post)
+      @new_comment = @new_post.comments.create(body: "comment body",
+                                              commentable_type: 'Post',
+                                              commentable_id: '1',
+                                              owner_type: 'Admin',
+                                              owner_id: '1')
+    end
+    context 'commenter' do
+
+      background do
+        @commenter = create(:commenter)
+        commenter_sign_in(@commenter)
+      end
+
+      scenario 'Adds a new comment' do
+        visit post_path(@new_post)
+        expect(page).to have_content('comment body')
+        click_link 'Reply'
+        fill_in('Body', with: 'a reply to comment')
+        click_button('Create Comment')
+        visit post_path(@new_post)
+        expect(page).to have_content('a reply to comment')
+        the_comment = @commenter.comments.first
+        expect(the_comment.body).to eq ('a reply to comment')
+        expect(the_comment.commentable_type).not_to eq('Post')
+
+
+      end
+    end
+
+  end
 end
 
