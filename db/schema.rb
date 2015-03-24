2ec54ae308dc6931718fccc3375776af2ff202b3
Newton <anthpaul0@gmail.com>
2015-03-23 21:51:00 -0500

    Remove commentable columns from comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318233433) do
+ActiveRecord::Schema.define(version: 20150324024817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,10 +55,8 @@ ActiveRecord::Schema.define(version: 20150318233433) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
-    t.integer  "commentable_id"
-    t.string   "commentable_type"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "ancestry"
     t.string   "owner_type"
     t.integer  "owner_id"
