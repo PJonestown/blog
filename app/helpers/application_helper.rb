3733841a83d32a739e4293187968726a18bcc01d
Newton <anthpaul0@gmail.com>
2015-03-28 00:08:52 -0500

    markdown helper for redcarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def markdown(text)
+    render_options = {
+      filter_html: true,
+      hard_wrap: true,
+      prettify: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(render_options)
+
+    extensions = {
+      strikethrough: true,
+      fenced_code_blocks: true,
+      superscript: true,
+      footnotes: true,
+      quote: true,
+      underline: true
+    }
+
+    Redcarpet::Markdown.new(renderer, extensions).render(text).html_safe
+  end
 end
