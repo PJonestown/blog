df95bb436c5bc7cf34c35f249490d4ec8e03ae58
Newton <anthpaul0@gmail.com>
2015-03-30 12:44:48 -0500

    Admin and unauth commenter tests for deleting comments

--- spec/features/deleting_comments_spec.rb
+++ spec/features/deleting_comments_spec.rb
@@ -18,10 +18,50 @@ feature 'deleting comments' do
     scenario 'deletes the comment' do
       visit post_path(@new_post)
       expect(page).to have_content(@comment.body)
-      click_link 'delete'
+      click_link 'delete comment'
       expect(current_path).to eq(post_path(@new_post))
       expect(page).not_to have_content(@comment.body)
     end
+  end
+
+  context 'an incorrect commenter' do
+
+    background do
+      visit post_path(@new_post)
+      click_link 'sign out'
+      visit post_path(@new_post)
+      @unauthorized_commenter = create(:other_commenter)
+      commenter_sign_in(@unauthorized_commenter)
+    end
+
+    scenario 'does not delete the comment' do
+      visit post_path(@new_post)
+      expect(page).to have_content(@comment.body)
+      expect(page).not_to have_link('delete comment')
+    end
+  end
+
+
+  context 'admin' do
+
+    background do
+      visit post_path(@new_post)
+      click_link 'sign out'
+      admin = create(:admin)
+      sign_in(admin)
+      visit post_path(@new_post)
+    end
+
+    scenario 'deletes the comment' do
+      expect(page).to have_content(@comment.body)
+      save_and_open_page
+      click_link 'delete comment'
+      expect(current_path).to eq(post_path(@new_post))
+      expect(page).not_to have_content(@comment.body)
+
+    end
+
+
   end
 end
 
