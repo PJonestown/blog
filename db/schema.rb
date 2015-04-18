03e06352f9e0beb7dc6a4430284998804fe89f24
Newton <anthpaul0@gmail.com>
2015-04-18 16:46:39 -0500

    Add draft column to post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325215034) do
+ActiveRecord::Schema.define(version: 20150418214240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,8 +78,9 @@ ActiveRecord::Schema.define(version: 20150325215034) do
     t.string   "title"
     t.text     "body"
     t.integer  "admin_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.boolean  "draft",      default: true
   end
 
   add_index "posts", ["admin_id"], name: "index_posts_on_admin_id", using: :btree
